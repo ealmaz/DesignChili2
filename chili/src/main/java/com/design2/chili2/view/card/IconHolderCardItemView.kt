8ec0e39aefc19b1da309d6279e235f1f8f389363
rkhasanova <rkhasanova@nurtelecom.kg>
2024-01-26 14:35:56 +0600

    removed unused import

--- chili/src/main/java/com/design2/chili2/view/card/IconHolderCardItemView.kt
+++ chili/src/main/java/com/design2/chili2/view/card/IconHolderCardItemView.kt
@@ -10,7 +10,6 @@ import android.view.LayoutInflater
 import android.view.View
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
-import androidx.core.content.ContextCompat
 import com.design2.chili2.R
 import com.design2.chili2.databinding.ChiliViewDiscountCardBinding
 import com.design2.chili2.extensions.color
