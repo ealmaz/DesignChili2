207760a4899d0e865a29bedfb2c17af2dbc19d5c
ealmaz <ealmaz@devcats.kg>
2024-07-05 16:40:44 +0600

    Implement chili3 module

---
+++ chili3/src/main/java/kg/devcats/chili3/view/cells/TestChili3CellView.kt
@@ -0,0 +1,7 @@
+package kg.devcats.chili3.view.cells
+
+import android.content.Context
+import com.design2.chili2.view.cells.BaseCellView
+
+class TestChili3CellView(context: Context) : BaseCellView(context) {
+}
