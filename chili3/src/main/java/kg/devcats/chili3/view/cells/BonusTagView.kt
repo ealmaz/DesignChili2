26de59177a589cf69e7fda4b0eb480b2012d6ba4
Adilbek Ashimov <aashimov@dc.devcats.kg>
2024-07-10 12:36:51 +0600

    (rebased main) fixing after rebase

--- chili3/src/main/java/kg/devcats/chili3/view/cells/BonusTagView.kt
+++ chili3/src/main/java/kg/devcats/chili3/view/cells/BonusTagView.kt
@@ -1,5 +1,6 @@
 package kg.devcats.chili3.view.cells
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.Drawable
