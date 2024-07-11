5495734bf719f06177d42519ec1c0e7591807812
Zaiyr <zaiyr.00@gmail.com>
2024-07-11 10:10:08 +0600

    ProductCardView impl

---
+++ chili3/src/main/java/kg/devcats/chili3/view/card/ProductCardView.kt
@@ -0,0 +1,154 @@
+package kg.devcats.chili3.view.card
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.annotation.DrawableRes
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.design2.chili2.extensions.dp
+import com.design2.chili2.extensions.drawable
+import com.design2.chili2.extensions.setImageByUrl
+import com.design2.chili2.view.card.BaseCardView
+import kg.devcats.chili3.R
+import kg.devcats.chili3.databinding.ChiliViewCardProductBinding
+import kg.devcats.chili3.extensions.visible
+
+class ProductCardView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = R.attr.productCardViewDefaultStyle,
+    defStyleRes: Int = R.style.Chili_CardViewStyle_ProductCardView
+) : BaseCardView(context, attrs, defStyleAttr, defStyleRes) {
+
+    private lateinit var vb: ChiliViewCardProductBinding
+
+    override val styleableAttrRes: IntArray = R.styleable.ProductCardView
+
+    override val rootContainer: View
+        get() = vb.clContainer
+
+    private var imageSize: Pair<Int, Int> = Pair(0, 0)
+
+    init { initView(context, attrs, defStyleAttr, defStyleRes) }
+
+    override fun inflateView(context: Context) {
+        vb = ChiliViewCardProductBinding.inflate(LayoutInflater.from(context), this, true)
+    }
+
+    override fun setupShimmeringViews() {
+        shimmeringPairs[vb.ivImage] = vb.viewIconShimmer
+        shimmeringPairs[vb.tvTitle] = vb.viewTitleShimmer
+        shimmeringPairs[vb.tvSubtitle] = vb.viewSubtitleShimmer
+        shimmeringPairs[vb.tvDescription] = vb.viewDescriptionShimmer
+        shimmeringPairs[vb.tvDiscount] = null
+    }
+
+    override fun TypedArray.obtainAttributes() {
+        getLayoutDimension(R.styleable.ProductCardView_android_layout_width, 0).let {
+            imageSize = it to imageSize.second
+        }
+        getLayoutDimension(R.styleable.ProductCardView_android_layout_height, 0).let {
+            imageSize = imageSize.first to it
+        }
+        getString(R.styleable.ProductCardView_productImage)?.let {
+            setProductImage(it)
+        }
+        setTitle(getText(R.styleable.ProductCardView_title))
+        getResourceId(R.styleable.ProductCardView_titleTextAppearance, -1)
+            .takeIf { it != -1 }?.let { setTitleTextAppearance(it) }
+        setSubtitle(getText(R.styleable.ProductCardView_subtitle))
+        getResourceId(R.styleable.ProductCardView_subtitleTextAppearance, -1)
+            .takeIf { it != -1 }?.let { setSubtitleTextAppearance(it) }
+        getResourceId(R.styleable.ProductCardView_subtitleBackgroundColor, R.drawable.chili_bg_product_subtitle)
+            .takeIf { it != -1 }?.let { setSubtitleBackground(it) }
+        setDescription(getText(R.styleable.ProductCardView_description))
+        getResourceId(R.styleable.ProductCardView_descriptionTextAppearance, -1)
+            .takeIf { it != -1 }?.let { setDescriptionTextAppearance(it) }
+        setDiscount(getText(R.styleable.ProductCardView_discount))
+        getResourceId(R.styleable.ProductCardView_discountTextAppearance, -1)
+            .takeIf { it != -1 }?.let { setDiscountTextAppearance(it) }
+        getResourceId(R.styleable.ProductCardView_discountBackgroundColor, R.drawable.chili_bg_product_discount_gradient)
+            .takeIf { it != -1 }?.let { setDiscountBackground(it) }
+    }
+
+    fun setProductImage(src: String) {
+        vb.ivImage.setImageByUrl(
+            url = src,
+            width = imageSize.first,
+            height = imageSize.second,
+            requestOptions = RequestOptions().transform(RoundedCorners(16.dp))
+        )
+    }
+
+    fun setTitle(text: CharSequence?) {
+        vb.tvTitle.text = text
+    }
+
+    fun setTitle(resId: Int) {
+        vb.tvTitle.setText(resId)
+    }
+
+    fun setTitleTextAppearance(resId: Int) {
+        vb.tvTitle.setTextAppearance(resId)
+    }
+
+    fun setSubtitle(text: CharSequence?) {
+        vb.tvSubtitle.apply {
+            this.text = text
+            if (!text.isNullOrEmpty()) visible()
+        }
+    }
+
+    fun setSubtitle(resId: Int) {
+        vb.tvSubtitle.apply {
+            setText(resId)
+            visible()
+        }
+    }
+
+    fun setSubtitleTextAppearance(resId: Int) {
+        vb.tvSubtitle.setTextAppearance(resId)
+    }
+
+    fun setSubtitleBackground(@DrawableRes drawableId: Int) {
+        vb.tvSubtitle.background = context.drawable(drawableId)
+    }
+
+    fun setDescription(text: CharSequence?) {
+        vb.tvDescription.text = text
+    }
+
+    fun setDescription(resId: Int) {
+        vb.tvDescription.setText(resId)
+    }
+
+    fun setDescriptionTextAppearance(resId: Int) {
+        vb.tvDescription.setTextAppearance(resId)
+    }
+
+    fun setDiscount(text: CharSequence?) {
+        vb.tvDiscount.apply {
+            this.text = text
+            if (!text.isNullOrEmpty()) visible()
+        }
+    }
+
+    fun setDiscount(resId: Int) {
+        vb.tvDiscount.apply {
+            setText(resId)
+            visible()
+        }
+    }
+
+    fun setDiscountTextAppearance(resId: Int) {
+        vb.tvDiscount.setTextAppearance(resId)
+    }
+
+    fun setDiscountBackground(@DrawableRes drawableId: Int) {
+        vb.tvDiscount.background = context.drawable(drawableId)
+    }
+
+}
