382dfd16cc67b689de6c4364d7ba9d3288a0a583
mshermamatov <mshermamatov@nurtelecom.kg>
2024-07-12 10:32:06 +0600

    ALGA-689_account_card_view

---
+++ app/src/main/java/com/design2/app/extensions/AccountCardViewExtensions.kt
@@ -0,0 +1,86 @@
+package com.design2.app.extensions
+
+import kg.devcats.chili3.R
+import kg.devcats.chili3.view.card.AccountCardView
+
+fun AccountCardView.setNonAuthorizedState(title: String?, subTitle: String?) {
+    setTitleIcon(drawableRes = null)
+    setTitleTextAppearance(R.style.Chili_H16_Primary_Bold)
+    setTitle(title)
+    setSubtitleTextAppearance(R.style.Chili_H14_Secondary)
+    setChevronVisibility(isChevronVisible = false)
+    setTitleAddition(charSequence = null)
+    setActionButtonIcon(drawableRes = null)
+    setToggleIconVisibility(isIconVisible = false)
+    setSubtitle(subTitle)
+}
+
+fun AccountCardView.setNonIdentifiedState(title: String?) {
+    setTitleIcon(com.design2.app.R.drawable.non_identified)
+    setTitleAddition(charSequence = null)
+    setChevronVisibility(isChevronVisible = false)
+    setTitle(title)
+    setActionButtonIcon(com.design2.app.R.drawable.chili_ic_card)
+    setToggleIconVisibility(isIconVisible = false)
+    setTitleTextAppearance(R.style.Chili_H14_AccentSecondary_700)
+    setSubtitle(charSequence = null)
+}
+
+fun AccountCardView.setIdentificationInProcessState(title: String?) {
+    setTitleIcon(com.design2.app.R.drawable.ic_dentification_in_process)
+    setTitleAddition(charSequence = null)
+    setChevronVisibility(isChevronVisible = false)
+    setActionButtonIcon(com.design2.app.R.drawable.chili_ic_card)
+    setToggleIconVisibility(isIconVisible = false)
+    setTitleTextAppearance(R.style.Chili_H14_Secondary_700)
+    setSubtitle(charSequence = null)
+    setTitle(title)
+}
+
+fun AccountCardView.setFavoritePaymentAmountAvailableState(
+    title: String?,
+    titleAddition: String?,
+    isToggleHiddenState: Boolean = false,
+    subTitle: String?
+) {
+    setTitleIcon(R.drawable.chili_ic_star)
+    setTitleTextAppearance(R.style.Chili_H14_Primary_Bold)
+    setTitleAdditionTextAppearance(R.style.Chili_H14_Value)
+    setChevronVisibility(isChevronVisible = true)
+    setTitleAddition(titleAddition)
+    setTitle(title)
+    setActionButtonIcon(com.design2.app.R.drawable.chili_ic_card)
+    setToggleIconVisibility(isIconVisible = true)
+    setToggleIconState(isToggleHiddenState)
+    setSubtitleTextAppearance(R.style.Chili_H14_Primary_Bold)
+    setSubtitle(subTitle)
+}
+
+fun AccountCardView.setFavoritePaymentAmountUnavailableState(
+    title: String?,
+    titleAddition: String?,
+    subTitle: String?
+) {
+    setTitleIcon(R.drawable.chili_ic_star)
+    setTitleTextAppearance(R.style.Chili_H14_Primary_Bold)
+    setTitleAdditionTextAppearance(R.style.Chili_H14_Value)
+    setChevronVisibility(isChevronVisible = true)
+    setTitleAddition(titleAddition)
+    setTitle(title)
+    setActionButtonIcon(com.design2.app.R.drawable.chili_ic_card)
+    setToggleIconVisibility(isIconVisible = false)
+    setSubtitleTextAppearance(R.style.Chili_H14_Warning_Bold)
+    setSubtitle(subTitle)
+}
+
+fun AccountCardView.setNoFavoritePaymentAmountState(title: String?, subTitle: String?) {
+    setTitleIcon(com.design2.app.R.drawable.chili_ic_star_outlined)
+    setTitleTextAppearance(R.style.Chili_H14_Primary_Bold)
+    setTitleAddition(charSequence = null)
+    setChevronVisibility(isChevronVisible = true)
+    setTitle(title)
+    setActionButtonIcon(com.design2.app.R.drawable.chili_ic_card)
+    setToggleIconVisibility(isIconVisible = false)
+    setSubtitleTextAppearance(R.style.Chili_H14_Warning_Bold)
+    setSubtitle(subTitle)
+}
