c4a0ea2b7f48db9076f98e9f3b64b6b9503f14f6
rkhasanova <rkhasanova@nurtelecom.kg>
2024-07-26 19:27:00 +0600

    refactored vh

--- app/src/main/java/com/design2/app/fragments/DefaultCartViewsFragment.kt
+++ app/src/main/java/com/design2/app/fragments/DefaultCartViewsFragment.kt
@@ -1,17 +1,18 @@
 package com.design2.app.fragments
 
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
 import androidx.annotation.StringRes
 import androidx.recyclerview.widget.RecyclerView
 import com.design2.app.base.BaseFragment
 import com.design2.app.databinding.FragmentDefaultCartBinding
-import com.design2.app.databinding.ItemViewNewCellBinding
+import com.design2.chili2.view.cells.BaseCellView
 import kotlin.random.Random
 
-class DefaultCartViewsFragment: BaseFragment<FragmentDefaultCartBinding>() {
+class DefaultCartViewsFragment : BaseFragment<FragmentDefaultCartBinding>() {
 
     private var titleResId: Int = -1
     private var title:String = ""
@@ -44,26 +45,26 @@ class DefaultCartViewsFragment: BaseFragment<FragmentDefaultCartBinding>() {
             DefaultCartItemData("Visa o!Dengi","···· 12421"),
             DefaultCartItemData("Банковский счет","···· 2341"),
             DefaultCartItemData("ELCART ЭЛКАП","···· 1234"),
-        )){selectedData ->
+        )){ selectedData ->
 
         }
         vb.rvDCart.adapter = adapter
 
     }
 
-    fun setTitleValue(value:String){
+    fun setTitleValue(value: String) {
         title = value
     }
 
-    fun setTitleResId(@StringRes id:Int){
+    fun setTitleResId(@StringRes id: Int) {
         titleResId = id
     }
 
-    fun setSubTitleValue(value:String){
+    fun setSubTitleValue(value: String) {
         subTitle = value
     }
 
-    fun setSubTitleResId(@StringRes id:Int){
+    fun setSubTitleResId(@StringRes id: Int) {
         subTitleResId = id
     }
 
@@ -85,30 +86,20 @@ class DefaultCartViewsFragment: BaseFragment<FragmentDefaultCartBinding>() {
 
 class ItemsDefaultCartListAdapter(
     var dataset: List<DefaultCartItemData>,
-    val selectData: (DefaultCartItemData) -> Unit) : RecyclerView.Adapter<DefaultCartViewVH>() {
+    val selectData: (DefaultCartItemData) -> Unit
+) : RecyclerView.Adapter<DefaultCartViewVH>() {
 
-    /* = listOf()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }*/
-
-    var selectedData: DefaultCartItemData ? = null
+    var selectedData: DefaultCartItemData? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DefaultCartViewVH {
-        val view = ItemViewNewCellBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return DefaultCartViewVH(view){ it ->
-            it.let { selectedData ->
-                this.selectedData = selectedData
-                this.selectData.invoke(selectedData)
-                notifyDataSetChanged()
-            }
+        return DefaultCartViewVH.create(parent.context) { selectedData ->
+            this.selectedData = selectedData
+            this.selectData.invoke(selectedData)
+            notifyDataSetChanged()
         }
     }
 
-    override fun getItemCount(): Int {
-        return dataset.size
-    }
+    override fun getItemCount(): Int = dataset.size
 
     override fun onBindViewHolder(holder: DefaultCartViewVH, position: Int) {
         holder.onBind(dataset[position], selectedData)
@@ -116,19 +107,20 @@ class ItemsDefaultCartListAdapter(
 }
 
 class DefaultCartViewVH(
-    val vb: ItemViewNewCellBinding,
-    val selectData: (DefaultCartItemData) -> Unit) : RecyclerView.ViewHolder(vb.root) {
+    val view: View,
+    val onCellSelected: (DefaultCartItemData) -> Unit
+) : RecyclerView.ViewHolder(view) {
+
     fun onBind(data: DefaultCartItemData, selectedData: DefaultCartItemData?) {
-        vb.item.apply {
+        (view as BaseCellView).apply {
             setTitle(data.title)
-            setTitleTextAppearance(kg.devcats.chili3.R.style.Chili_H16_Primary_Bold)
             setSubtitle(data.subtitle)
-            setSubtitleTextAppearance(com.design2.chili2.R.style.Chili_H9_Secondary)
-            when{
+            when {
                 (data.id == selectedData?.id) -> {
                     setChevron(kg.devcats.chili3.R.drawable.ic_selected_radio_button)
                     setChevronColor(kg.devcats.chili3.R.color.radio_button_selected_border_color)
                 }
+
                 else -> {
                     setChevron(kg.devcats.chili3.R.drawable.ic_radiobutton)
                     setChevronColor(kg.devcats.chili3.R.color.radio_button_default_border_color)
@@ -136,8 +128,19 @@ class DefaultCartViewVH(
             }
 
             setOnClickListener {
-                selectData.invoke(data)
+                onCellSelected.invoke(data)
             }
+        }
+    }
+
+    companion object {
+        fun create(context: Context, onSelect: (DefaultCartItemData) -> Unit): DefaultCartViewVH {
+            val view = BaseCellView(context = context, ).apply {
+                layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+                setTitleTextAppearance(kg.devcats.chili3.R.style.Chili_H16_Primary_Bold)
+                setSubtitleTextAppearance(com.design2.chili2.R.style.Chili_H9_Secondary)
+            }
+            return DefaultCartViewVH(view) { onSelect.invoke(it) }
         }
     }
 }
