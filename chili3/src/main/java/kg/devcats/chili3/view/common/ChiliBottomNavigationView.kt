262876e372d47bfdd724e8582d09a4f59620192c
mshermamatov <mshermamatov@nurtelecom.kg>
2024-08-01 19:09:35 +0600

    ALGA-1091_animate_bottom_navigation

--- chili3/src/main/java/kg/devcats/chili3/view/common/ChiliBottomNavigationView.kt
+++ chili3/src/main/java/kg/devcats/chili3/view/common/ChiliBottomNavigationView.kt
@@ -1,9 +1,14 @@
 package kg.devcats.chili3.view.common
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.design2.chili2.extensions.drawable
 import com.design2.chili2.extensions.setOnSingleClickListener
@@ -14,6 +19,13 @@ import kg.devcats.chili3.databinding.ChiliViewBottomNavigationBinding
 class ChiliBottomNavigationView : ConstraintLayout {
 
     private lateinit var vb: ChiliViewBottomNavigationBinding
+    private var animatedItemScale = DEFAULT_ANIMATED_ITEM_SCALE
+    private var animatedItemDuration = DEFAULT_ANIMATED_ITEM_DURATION
+    private var isItemAnimated = false
+    private val mapOfItemAnimations by lazy { mutableMapOf<View, AnimatorSet>() }
+
+    private var onItemSelectedListener: NavigationBarView.OnItemSelectedListener? = null
+    private var onIconClick: (() -> Unit)? = null
 
     constructor(context: Context) : super(context) {
         inflateViews()
@@ -41,10 +53,17 @@ class ChiliBottomNavigationView : ConstraintLayout {
                 ?.let { setIcon(it) }
             getResourceId(R.styleable.ChiliBottomNavigationView_background, R.drawable.chili_rounded_nav_bar)
                 .takeIf { it != -1 }?.let { setBottomNavBackground(it) }
+            setItemsAnimation(getBoolean(R.styleable.ChiliBottomNavigationView_isAnimated, false))
+            getFloat(R.styleable.ChiliBottomNavigationView_animationScale, -1f)
+                .takeIf { it < 0 }?.let(::setAnimatedItemScale)
+            getInt(R.styleable.ChiliBottomNavigationView_android_animationDuration, -1)
+                .takeIf { it != -1 }?.let(::setAnimatedItemDuration)
+
             vb.bottomNav.itemIconTintList = null
             vb.bottomNav.removeBackgroundForItem(2)
             vb.viewBackground.setOnClickListener(null)
             recycle()
+            setupItemSelectedAnimation()
         }
     }
 
@@ -74,11 +93,7 @@ class ChiliBottomNavigationView : ConstraintLayout {
     }
 
     fun setOnIconClickListener(action: () -> Unit) {
-        vb.ivIcon.setOnSingleClickListener(action)
-    }
-
-    fun setOnItemSelectedListener(listener: NavigationBarView.OnItemSelectedListener) {
-        vb.bottomNav.setOnItemSelectedListener(listener)
+        onIconClick = action
     }
 
     fun getSelectedItemId(): Int {
@@ -89,4 +104,79 @@ class ChiliBottomNavigationView : ConstraintLayout {
         return vb.bottomNav
     }
 
+    fun setItemsAnimation(isAnimated: Boolean) {
+        isItemAnimated = isAnimated
+    }
+
+    fun setAnimatedItemScale(scale: Float?) {
+        scale?.let {
+            if (scale in 1f..3f) animatedItemScale = scale
+        }
+        mapOfItemAnimations.clear()
+    }
+
+    fun setAnimatedItemDuration(duration: Int?) {
+        duration?.let {
+            if (it in 1..1000) animatedItemDuration = duration.toLong()
+        }
+        mapOfItemAnimations.clear()
+    }
+
+    fun setOnItemSelectedListener(listener: NavigationBarView.OnItemSelectedListener) {
+        this.onItemSelectedListener = listener
+    }
+
+    private fun setupItemSelectedAnimation() = with(vb.bottomNav){
+        setupOnIconClickAnimation()
+        setOnItemSelectedListener { menuItem ->
+            if(!isItemAnimated) {
+                onItemSelected(menuItem)
+                return@setOnItemSelectedListener true
+            }
+
+            val selectedItemView = findViewById<View>(menuItem.itemId)
+            (mapOfItemAnimations[selectedItemView] ?: applyBounceAnimation(selectedItemView)).start()
+            onItemSelected(menuItem)
+            true
+        }
+    }
+
+    private fun onItemSelected(menuItem: MenuItem) {
+        onItemSelectedListener?.onNavigationItemSelected(menuItem)
+    }
+
+    private fun setupOnIconClickAnimation() = with(vb.ivIcon) {
+        setOnSingleClickListener {
+            if(!isItemAnimated) {
+                onIconClick?.invoke()
+                return@setOnSingleClickListener
+            }
+            (mapOfItemAnimations[this] ?: applyBounceAnimation(this)).start()
+            onIconClick?.invoke()
+        }
+    }
+
+    private fun applyBounceAnimation(view: View): AnimatorSet {
+        val scaleXUp = ObjectAnimator.ofFloat(view, "scaleX", 1.0f, animatedItemScale)
+        val scaleYUp = ObjectAnimator.ofFloat(view, "scaleY", 1.0f, animatedItemScale)
+
+        val scaleXDown = ObjectAnimator.ofFloat(view, "scaleX", animatedItemScale, 1.0f)
+        val scaleYDown = ObjectAnimator.ofFloat(view, "scaleY", animatedItemScale, 1.0f)
+
+        scaleXDown.interpolator = OvershootInterpolator()
+        scaleYDown.interpolator = OvershootInterpolator()
+
+        val animatorSet = AnimatorSet().apply {
+            play(scaleXUp).with(scaleYUp)
+            play(scaleXDown).with(scaleYDown).after(scaleXUp)
+            duration = animatedItemDuration
+        }
+        mapOfItemAnimations[view] = animatorSet
+        return animatorSet
+    }
+
+    companion object {
+        private const val DEFAULT_ANIMATED_ITEM_SCALE = 1.3F
+        private const val DEFAULT_ANIMATED_ITEM_DURATION = 68L
+    }
 }
