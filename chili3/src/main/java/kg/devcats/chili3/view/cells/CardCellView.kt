e91f60ee108dc27fe95986332e37a8529361c5dc
Adilbek Ashimov <aashimov@dc.devcats.kg>
2024-09-23 14:26:43 +0600

    added logic to remove bottom constraint

--- chili3/src/main/java/kg/devcats/chili3/view/cells/CardCellView.kt
+++ chili3/src/main/java/kg/devcats/chili3/view/cells/CardCellView.kt
@@ -11,6 +11,7 @@ import androidx.annotation.StringRes
 import androidx.annotation.StyleRes
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
 import com.design2.chili2.extensions.dpF
 import com.design2.chili2.extensions.setImageByUrl
 import com.design2.chili2.extensions.setImageOrHide
@@ -133,6 +134,18 @@ class CardCellView @JvmOverloads constructor(
                     }
                 )
             }
+        }
+    }
+
+    fun removeDataContainerBottomConstraint() {
+        vb.dataContainer.updateLayoutParams<LayoutParams> {
+            bottomToBottom = LayoutParams.UNSET
+        }
+    }
+
+    fun removeIconBottomConstraint() {
+        vb.ivIcon.updateLayoutParams<LayoutParams> {
+            bottomToBottom = LayoutParams.UNSET
         }
     }
 
