660062f4057f9fbcd90339bf52bd4848c7ec6925
dc-tnariev <tnariev@macadmins-Laptop.local>
2025-01-20 11:55:42 +0600

    fixed story page transformer

--- chili/src/main/java/com/design2/chili2/view/stories/adapter/ChiliStoryPageTransformer.kt
+++ chili/src/main/java/com/design2/chili2/view/stories/adapter/ChiliStoryPageTransformer.kt
@@ -2,13 +2,18 @@ package com.design2.chili2.view.stories.adapter
 
 import android.view.View
 import androidx.viewpager2.widget.ViewPager2
+
 class ChiliStoryPageTransformer : ViewPager2.PageTransformer {
 
     override fun transformPage(view: View, position: Float) {
         val deltaY = 0.5F
 
-        view.pivotX = if (position < 0F) view.width.toFloat() else 0F
-        view.pivotY = view.height * deltaY
-        view.rotationY = 45F * position
+        if (position >= -1 && position <= 1) {
+            view.pivotX = if (position < 0F) view.width.toFloat() else 0F
+            view.pivotY = view.height * deltaY
+            view.rotationY = 45F * position
+        } else {
+            view.rotationY = 0F
+        }
     }
 }
