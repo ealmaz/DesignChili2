7fb5e0ea0f1fcabbbe62c4c3346aeb0610da7f65
dc-tnurdinov <tnurdinov@devcats.kg>
2025-02-25 09:18:28 +0600

    OM-30652(feature): fix code review

--- chili3/src/main/java/kg/devcats/chili3/view/card/CardHorizontalDivider.kt
+++ chili3/src/main/java/kg/devcats/chili3/view/card/CardHorizontalDivider.kt
@@ -1,22 +1,23 @@
 package kg.devcats.chili3.view.card
 
 import android.content.Context
-import android.content.res.TypedArray
 import android.text.TextUtils
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.Space
 import android.widget.TextView
 import androidx.annotation.ColorRes
 import androidx.annotation.StyleRes
 import com.design2.chili2.extensions.color
 import com.design2.chili2.extensions.dp
 import com.design2.chili2.view.card.BaseCardView
-import com.google.android.material.divider.MaterialDivider
 import kg.devcats.chili3.R
 import kg.devcats.chili3.databinding.ChiliViewCardHorizontalDividerBinding
+import kg.devcats.chili3.extensions.getColorFromAttr
 
 class CardHorizontalDivider @JvmOverloads constructor(
     context: Context,
@@ -26,38 +27,58 @@ class CardHorizontalDivider @JvmOverloads constructor(
 ) : BaseCardView(context, attrs, defStyleAttr, defStyleRes) {
 
     private lateinit var vb: ChiliViewCardHorizontalDividerBinding
-    override val styleableAttrRes: IntArray = R.styleable.CardHorizontalDivider
-    init { initView(context, attrs, defStyleAttr, defStyleRes) }
+
+    override val rootContainer: View
+        get() = vb.root
+
+    override val styleableAttrRes: IntArray
+        get() = R.styleable.CardHorizontalDivider
+
+    init {
+        initView(context, attrs, defStyleAttr, defStyleRes)
+    }
 
     override fun inflateView(context: Context) {
         vb = ChiliViewCardHorizontalDividerBinding.inflate(LayoutInflater.from(context), this, true)
     }
 
-    override fun TypedArray.obtainAttributes() {
-    }
-
     fun setSections(sectionList: List<DividerCardSection>) {
         sectionList.forEachIndexed { index, section ->
-            val sectionContainer = createLinearLayoutContainer(orientation = LinearLayout.HORIZONTAL)
-            val contentContainer = createLinearLayoutContainer(orientation = LinearLayout.VERTICAL)
-            val titleTV = section.title?.let(::createTextView)
-            val subTitle = section.subtitle?.let(::createTextView)
+            val title = section.title?.let(::createTextView)
+            val subtitle = section.subtitle?.let(::createTextView)
             val verticalDivider = index.takeIf { it > 0 && it < sectionList.size }?.let {
                 createVerticalDivider()
             }
 
-            titleTV?.let(contentContainer::addView)
-            subTitle?.let(contentContainer::addView)
-            verticalDivider?.let(sectionContainer::addView)
-            sectionContainer.addView(contentContainer)
-
-            vb.sectionsContainer.addView(sectionContainer)
+            vb.sectionsContainer.addView(addViewsToSection(title, subtitle, verticalDivider))
         }
     }
 
-    private fun createVerticalDivider() = MaterialDivider(context).apply {
+    private fun addViewsToSection(
+        title: TextView?,
+        subtitle: TextView?,
+        verticalDivider: View?
+    ): LinearLayout {
+        val sectionContainer = createLinearLayoutContainer(orientation = LinearLayout.HORIZONTAL)
+        val contentContainer = createLinearLayoutContainer(orientation = LinearLayout.VERTICAL)
+        val spacer = subtitle.takeIf { it != null }?.let { createSpacer(ViewGroup.LayoutParams.WRAP_CONTENT, 6.dp) }
+
+        title?.let(contentContainer::addView)
+        spacer?.let(contentContainer::addView)
+        subtitle?.let(contentContainer::addView)
+        verticalDivider?.let(sectionContainer::addView)
+        sectionContainer.addView(contentContainer)
+
+        return sectionContainer
+    }
+
+    private fun createSpacer(width: Int, height: Int) = Space(context).apply {
+        layoutParams = ViewGroup.LayoutParams(width, height)
+    }
+
+    private fun createVerticalDivider() = View(context).apply {
         layoutParams = ViewGroup.LayoutParams(1.dp, ViewGroup.LayoutParams.MATCH_PARENT)
-        setDividerColorResource(R.color.c_E0E0E5)
+        this.setBackgroundColor(context.getColorFromAttr(R.attr.ChiliCardHorizontalDividerColorDivider))
     }
 
     private fun createLinearLayoutContainer(orientation: Int) = LinearLayout(context).apply {
@@ -72,7 +93,6 @@ class CardHorizontalDivider @JvmOverloads constructor(
 
     private fun createTextView(textView: DividerTextView): TextView = TextView(context).apply {
         text = textView.text
-        maxEms = 8
         maxLines = 1
         ellipsize = TextUtils.TruncateAt.END
         setTextAppearance(textView.textAppearance)
